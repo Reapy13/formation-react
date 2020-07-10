2a80c4b69c9139ffe5c2953ffa255b6a655fd976
Reapy13 <hugo.fauchery@gmail.com>
2020-07-10 11:32:59 +0200

    Adds Home component

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Login />
-      </header>
+      <Home />
+      <Login />
     </div>
   );
 }
