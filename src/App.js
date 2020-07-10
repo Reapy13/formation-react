ec4f1602770f82e45780f9e0f0d6b67a853a458f
Reapy13 <hugo.fauchery@gmail.com>
2020-07-10 12:15:49 +0200

    Adds the route for home and login.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
 import Home from './components/Home';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <Login />
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Route path="/home" render={() => (<Home />)}/>
+          <Route path="/login" render={() => (<Login />)}/>
+        </header>
+      </div>
+    </Router>
   );
 }
 
